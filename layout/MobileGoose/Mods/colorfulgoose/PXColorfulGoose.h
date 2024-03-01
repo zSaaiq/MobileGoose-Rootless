f14cfafbef2b05524719320c6f682bca53d1461b
zSaaiq <kevin.felix90@gmx.de>
2024-03-02 00:50:47 +0100

    xds

--- layout/MobileGoose/Mods/colorfulgoose/PXColorfulGoose.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <MobileGoose.h>
-
-@interface PXColorfulGoose : NSObject<MGMod> {
-	CGFloat hue;
-}
-- (void)handleFrameInState:(MGGooseFrameState)state;
-@property (nonatomic, assign) BOOL enabled;
-@end
